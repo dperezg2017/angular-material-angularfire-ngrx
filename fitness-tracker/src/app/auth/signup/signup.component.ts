39b2d17f92d591ab162c9398aeaa2486882e47ed
dperezg2017 <dperezg2017@gmail.com>
2019-11-29 02:27:29 -0500

    2

--- fitness-tracker/src/app/auth/signup/signup.component.ts
+++ fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -8,10 +8,15 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
 
+  maxDate;
+
   constructor() { }
 
   ngOnInit() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
+
 onSubmit(form: NgForm){
   console.log(form);
 }
